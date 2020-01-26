8f1e26d0448a4bfa9fe3a94e934944d1ba17775f
ShreyaJingade <shreyajingade123@gmail.com>
2020-01-26 20:52:21 +0530

    https://github.com/ShreyaJingade/assignsubmission.git

---
+++ program1.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i, n;
+			Console.WriteLine("Enter the input array size");
+			n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Size is " + n);
+			int[] a = new int[n];
+			Console.WriteLine("Enter the input numbers");
+			for (i = 0; i < n; i++)
+			{
+				a[i] = Convert.ToInt32(Console.ReadLine());
+			}
+			for (i = 0; i < n; i++)
+			{
+				if (a[i] % 2 == 0)
+					a[i]--;
+				else
+					a[i]++;
+			}
+			for(i=0;i<n; i++) 
+			{
+				Console.WriteLine(a[i]);
+			}
+			
+		}
+    }
+}
